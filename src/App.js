9aa02cca8ce20c12fe2162d52e19c3bb61999ced
andreidrobota <drobotaandrei@gmail.com>
2024-05-30 11:30:41 +0000

    Added React Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-      <p>It works</p>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
